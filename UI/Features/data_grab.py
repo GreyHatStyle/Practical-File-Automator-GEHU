6a50aec4366f69c230feaae737d628592965de7e
Manas Bisht <manasbisht1142004@gmail.com>
2024-01-26 17:12:47 +0530

    Added Saving Feature details.txt

--- UI/Features/data_grab.py
+++ UI/Features/data_grab.py
@@ -1,29 +1,47 @@
 
-from PyQt6.QtWidgets import QLineEdit, QTextEdit
+from PyQt6.QtWidgets import QLineEdit
+from tkinter import messagebox
 from os import getcwd
 
 class Data_Put_Handle:
     def __init__(self) -> None:
-        pass
+        self.path = getcwd()
+
+        # Setting Actual Path (Relative Path didn't work for some reason)
+        self.details_path = f"{self.path}\\UI\\Features\\Data\\details.txt"
+        self.settings_path = f"{self.path}\\UI\\Features\\Data\\settings.txt"
+        
+        # Strucuring Data from setting.txt
+        self.data_dct = self.get_settingData()
     
+    # Function for putting details.txt in GUI
     def put_details(self, name :QLineEdit, roll :QLineEdit, sec :QLineEdit,
                     course: QLineEdit):
-        path = getcwd()
-        path = f"{path}\\UI\\Features\\Data\\details.txt"
-        with open(path, "r") as ip:
+        
+        with open(self.details_path, "r") as ip:
             lst = ip.read().split('#')
 
         name.setText(lst[0])
         roll.setText(lst[1])
         sec.setText(lst[2])
         course.setText(lst[3])
+        
+
+    # Function for updating details.txt in Memory
+    def save_detailstxt(self, name: str, roll: str, sec: str, course: str):
+        ans = messagebox.askyesno(message="Do you want to Save Changes?", title="Save Changes?")
+        if ans:
+            with open(self.details_path, "w") as ip:
+                ip.write(f"{name}#{roll}#{sec}#{course}")
+
+        
 
 
+    # Function for putting setting memory data in GUI
     def get_settingData(self)->dict:
         data = {}
-        path = getcwd()
-        path = f"{path}\\UI\\Features\\Data\\settings.txt"
-        with open(path, "r") as ip:
+
+        with open(self.settings_path, "r") as ip:
             data['header_footer'] = ip.readline().replace('\n','').split('#')
             data['bef_ip'] = ip.readline().replace('\n','').split('#')
             data['bef_op'] = ip.readline().replace('\n','').split('#')
@@ -32,7 +50,7 @@ class Data_Put_Handle:
         return data
     
     def put_settingData(self, headFoot :list, befip: list, befop: list, genfont: list):
-        data = self.get_settingData()
+        data = self.data_dct
         
         # header and footer
         headFoot[0].setText(data['header_footer'][0])
@@ -49,5 +67,6 @@ class Data_Put_Handle:
         # general fontstyle, general fontsize
         genfont[0].setText(data['gen_font'][0])
         genfont[1].setText(data['gen_font'][1])
+
 
         
